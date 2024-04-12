d272f351af916bb6375dc4e6c150ff0e45372e70
Roman Khatri <learncipher01@gmail.com>
2024-04-12 09:00:33 +0545

    Add Program file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Animal A1 = new();
+Animal A2 = new();
+
+A1.species = "Dog";
+A1.breed = "Siberian Husky";
+A1.name = "Shadow";
+A1.height = 22;
+A1.weight = 48;
+
+A2.species = "Fish";
+A2.breed = "Arowna";
+A2.name = "Lucky";
+A2.height = 7;
+A2.weight = 1;
+
+A1.Printdetails();
+A2.Printdetails();
